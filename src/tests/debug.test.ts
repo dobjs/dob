5dabb64cb107380798247ff2e10cbef65cc0c63f
ascoders <576625322@qq.com>
2017-09-26 19:46:48 +0800

    test nested object

--- src/tests/debug.test.ts
+++ src/tests/debug.test.ts
@@ -17,3 +17,22 @@ test("debug", t => {
   return Promise.resolve()
     .then(() => t.true(data === "abcd"))
 })
+
+test("nested debug", t => {
+  startDebug()
+
+  let data = ""
+  const dynamicObj = observable({
+    user: { name: "b" }
+  })
+
+  data += "a"
+  observe(() => data += dynamicObj.user.name)
+  data += "c"
+  dynamicObj.user.name = "d"
+
+  stopDebug()
+
+  return Promise.resolve()
+    .then(() => t.true(data === "abcd"))
+})
